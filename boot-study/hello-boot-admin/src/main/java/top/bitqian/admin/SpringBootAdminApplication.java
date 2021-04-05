42fdca06da5af15f2c7e2ea1e592e523a9f895a9
sevenyoungairye <849033901@qq.com>
2021-04-05 22:07:52 +0800

    新增监控模块可视化。

---
+++ boot-study/hello-boot-admin/src/main/java/top/bitqian/admin/SpringBootAdminApplication.java
@@ -0,0 +1,23 @@
+package top.bitqian.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author echo lovely
+ * @date 2021/4/5 21:54
+ * @description boot admin to actuator many services..
+ */
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class SpringBootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+
+}
